2666d34d91f9a5bce4cab08defa12fe7b72f29be
Ketan Chaudhari <143803358+ketanc1104@users.noreply.github.com>
2023-10-18 15:50:50 +0530

    Add files via upload

---
+++ Pattern/Print.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+
+{
+	printf("My Name Is Ketan\n");
+	printf("I am 23 Years Old \n");
+	printf("My School Name Is Sungrace School \n");
+	
+}
---
+++ Pattern/pattern 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	printf(" - - -\n");
+	printf("|     |\n");
+	printf("K     |\n");
+	printf("E     |\n");
+	printf("T     |\n");
+	printf("A     |\n");
+	printf("N     |\n");
+	printf("|     |\n");
+	printf(" - - -");
+}
